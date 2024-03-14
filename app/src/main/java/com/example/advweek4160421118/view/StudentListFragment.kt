c3ed982982e15b352478b206720847fae845f452
KevinTegar <bagas.kevin1234@gmail.com>
2024-03-14 17:17:22 +0700

    model,view,and viewmodel

--- app/src/main/java/com/example/advweek4160421118/view/StudentListFragment.kt
+++ app/src/main/java/com/example/advweek4160421118/view/StudentListFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.advweek4160421118.R
+import com.example.advweek4160421118.databinding.FragmentStudentListBinding
+import com.example.advweek4160421118.viewmodel.ListViewModel
 
 
 /**
@@ -14,14 +19,43 @@ import com.example.advweek4160421118.R
  * create an instance of this fragment.
  */
 class StudentListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter  = StudentListAdapter(arrayListOf())
+    private lateinit var binding: FragmentStudentListBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false)
+        binding = FragmentStudentListBinding.inflate(inflater,container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.txtError?.visibility = View.VISIBLE
+            } else {
+                binding.txtError?.visibility = View.GONE
+            }
+        })
+
+    }
+
 
 }
